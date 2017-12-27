43433749057ca26cfbe7fb2dab6975f83f996f06
Matthias <matsigiga@gmail.com>
2017-12-27 18:03:46 +0100

    check in

---
+++ Assets/Scripts/FoodSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour {
+
+    public GameObject FoodPrefab;
+    public float SpawnFrequency;
+
+    private float currentTime = 0;
+	
+	// Update is called once per frame
+	void Update () {
+        currentTime += Time.deltaTime;
+
+        if(currentTime > SpawnFrequency) {
+            currentTime = 0;
+            spawnFood();
+        }
+	}
+
+    private void spawnFood() {
+
+        float x = Random.Range(-5.95f, 5.95f);
+        float y = Random.Range(-4.3f, 4.3f);
+
+        FoodPrefab.transform.position = new Vector3(x, y, 0);
+        Instantiate(FoodPrefab);
+    }
+}
