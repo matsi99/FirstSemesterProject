28feed0ae1c024e4a761c2b618c6817ceee165f4
Matthias <matsigiga@gmail.com>
2018-01-10 17:03:51 +0100

    reset for new concept

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float MovementSpeed;
+    public Camera Cam;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private void Update() {
+        getInput();
+        cameraFollow();
+    }
+
+    private void getInput() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void cameraFollow() {
+        Cam.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Cam.transform.position.z);
+    }
+
+    private void FixedUpdate() {
+        move();
+    }
+
+    private void move() {
+        float xMovement = horizontalInput * Time.deltaTime * MovementSpeed;
+        float yMovement = verticalInput * Time.deltaTime * MovementSpeed;
+
+        transform.Translate(new Vector3(xMovement, yMovement, transform.position.z));
+    }
+
+}
