994cc8fb55d86a9256269c67522f00b301015fcb
Matthias <matsigiga@gmail.com>
2018-01-24 12:10:04 +0100

    add enemies, health, highscore, startMenu, gameOver Screen

---
+++ Assets/Scripts/EnemyManagement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManagement : MonoBehaviour {
+
+
+    public EnemyBehaviour enemy;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+
+    private float refTime;
+
+	// Use this for initialization
+	void Start () {
+        resetTimer();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (refTime < Time.time)
+        {
+            resetTimer();
+            Instantiate(enemy);
+        }  
+	}
+
+    private void resetTimer() {
+        refTime = Time.time + Random.Range(minSpawnTime,maxSpawnTime);
+    }
+}
